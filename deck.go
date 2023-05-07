a2b9c6c0928a508c027ea23a5e108f3b6ea49cfb
Lidu <lidiaagatakongiel@gmail.com>
2023-05-07 21:22:25 +0200

    Saving Data to the Hard Drive

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of 'deck'
 // witch is a slice of strings
@@ -30,4 +34,12 @@ func (d deck) print() {
 //tworzymy funkcję deal, która zwróci dwie wartości typu "deck"
 func deal(d deck, handSize int) (deck, deck) {
 	return d [:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	 return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
